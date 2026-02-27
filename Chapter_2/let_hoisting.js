91c40d6cbd0b348c8a296c068c621cadd0a6dded
Shubhangi888More <meetshubhangimore@gmail.com>
2026-02-27 23:59:26 +0530

    Add Chapter_2 files for hoisting examples

---
+++ Chapter_2/let_hoisting.js
@@ -0,0 +1,20 @@
+// console.log(username) //tdz starts
+// let username= "nita"  //tdz ends
+// console.log(username)
+
+// let a = 10
+// if (true){
+//     console.log(a);
+//     let a = 19;
+//     console.log(a)
+// }
+//console.log(a)     //op - error
+
+let a = 10
+if (true){
+   // console.log(a);
+    let a = 19;
+    console.log(a)
+}
+console.log(a)    //op = 19 10 ...bocz both declaration are diff - global and local so its allow
+                  //else let not allows redeclration
